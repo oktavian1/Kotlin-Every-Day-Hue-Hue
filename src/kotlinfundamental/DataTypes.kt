95db043a71d57802f84c09464ba3eef143684e50
ilham oktavian <ilham.oktavian@jakpat.net>
2020-10-20 20:57:40 +0700

    first commit

---
+++ src/kotlinfundamental/DataTypes.kt
@@ -0,0 +1,17 @@
+package kotlinfundamental
+
+fun main() {
+    /*
+        macam datatypes character, string, array, numbers, dan booleans
+     */
+
+    // var atau val . var bisa mengubah nilai yang sudah diinsialisasikan contoh
+
+    var name: String = "ilham"
+    name = "japra"
+
+    println(name)
+
+    val age = 22
+    println("my name is $name and my age is $age")
+}
